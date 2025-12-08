09a1419c3cff2b48c2e9074764822b29c7f002aa
Devon Adkins <adkins.devon@hotmail.com>
2025-12-08 09:55:32 +0200

    feat: production backend configuration

--- backend.tf
+++ backend.tf
@@ -1,12 +1,8 @@
-// For more information on how to secure your Terraform State you can vist the below link
-// https://developer.hashicorp.com/terraform/language/backend/s3
-// We are going to make use of State Locking, this will ensure that we protect the state by only allowing one user to make changes to your infrastructure at any given point of time
-
-
+# Production Backend Configuration
 terraform {
   backend "s3" {
     bucket         = "deadki-terraform-production-state-bucket"
-    key            = "terraform-production.tfstate"
+    key            = "terraform-prod.tfstate"
     region         = "us-east-2" 
     encrypt        = true
     dynamodb_table = "terraform-state-lock"             
