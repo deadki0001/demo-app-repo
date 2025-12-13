081bc48e3f4fc7cb8df4e0c6506aefb14d0ab8c5
Devon Adkins <adkins.devon@hotmail.com>
2025-12-13 14:46:20 +0200

    feature/DEVOPS-001: Deploying nonprod terraform state

--- backend.tf
+++ backend.tf
@@ -1,5 +1,5 @@
 // For more information on how to secure your Terraform State you can vist the below link
-// https://developer.hashicorp.com/terraform/language/backend/s3
+// https://developer.hashicorp.com/terraform/language/backend/s3#
 // We are going to make use of State Locking, this will ensure that we protect the state by only allowing one user to make changes to your infrastructure at any given point of time
 
 
