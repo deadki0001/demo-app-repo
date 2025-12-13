8a529d05f5e51b30fc16ef47be3bed4385df8ee8
Devon Adkins <adkins.devon@hotmail.com>
2025-12-13 20:03:52 +0200

    feature/DEVOPS-001: creating nonprod terraform state

--- ecr.tf
+++ ecr.tf
@@ -2,9 +2,42 @@
 // For a complete list of configuration items you are welcome to visit the above link.
 // For a more ind depth read on Amazon ECR, I have included a readme.txt with the AWS Whitepaper which will provide you with any additional context.
 
+data "aws_caller_identity" "current" {}
+
 resource "aws_kms_key" "ecr_kms" {
   description             = "KMS key for ECR encryption (nonprod)"
   deletion_window_in_days = 7
+  enable_key_rotation     = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "EnableRootPermissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowECRUseOfKey"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecr.amazonaws.com"
+        }
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 resource "aws_ecr_repository" "demo_ecr_repo" {
