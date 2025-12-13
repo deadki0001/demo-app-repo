9775b6c6dd14636fe8deee77b3a055ba00ed9095
Devon Adkins <adkins.devon@hotmail.com>
2025-12-13 19:56:21 +0200

    feature/DEVOPS-001: fixing checkov

--- ecr.tf
+++ ecr.tf
@@ -2,12 +2,21 @@
 // For a complete list of configuration items you are welcome to visit the above link.
 // For a more ind depth read on Amazon ECR, I have included a readme.txt with the AWS Whitepaper which will provide you with any additional context.
 
+resource "aws_kms_key" "ecr_kms" {
+  description             = "KMS key for ECR encryption (nonprod)"
+  deletion_window_in_days = 7
+}
+
 resource "aws_ecr_repository" "demo_ecr_repo" {
   name                 = "demo-app-images"
   image_tag_mutability = "MUTABLE"
+
+  encryption_configuration {
+    encryption_type = "KMS"
+    kms_key         = aws_kms_key.ecr_kms.arn
+  }
 
   image_scanning_configuration {
     scan_on_push = true
   }
 }
-#
