7efa4cc56fee7368aceed2d9fac28c86a1044b31
Devon Adkins <adkins.devon@hotmail.com>
2025-12-13 15:29:26 +0200

    feature/DEVOPS-001: Deploying nonprod terraform state

--- security-group.tf
+++ security-group.tf
@@ -12,6 +12,8 @@
 // It allows you to control all external traffic to Resources in AWS, including your EC2 Instance.
 // Below we are allowing HTTP and HTTPS traffic
 
+#checkov:skip=CKV2_AWS_5:Security groups defined for when EC2/RDS resources are uncommented
+#checkov:skip=CKV_AWS_260:Demo environment requires public HTTP access for web application testing
 resource "aws_security_group" "application_security_group" {
   name        = "application-security-group"
   description = "Allow HTTP and HTTPS inbound traffic"
@@ -33,45 +35,44 @@ resource "aws_security_group" "application_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  #checkov:skip=CKV_AWS_382:Outbound internet access required for package updates and external API calls
   egress {
     description = "Allow all outbound connections"
     from_port   = 0
     to_port     = 0
-    protocol    = "-1" // -1 refers to All Outbound Traffic being allowed, Across both TCP & UDP Suites.
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
     Name = "application-security-group"
   }
-
 }
 
-// Database Security Group
-
+#checkov:skip=CKV2_AWS_5:Security groups defined for when EC2/RDS resources are uncommented
 resource "aws_security_group" "database_security_group" {
   name        = "database-security-group"
   description = "Allow MySQL traffic from Web Application Server"
   vpc_id      = aws_vpc.demo_application_vpc.id
 
   ingress {
-    description = "Allow MySQL Traffic from the above Web App Security Group"
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
+    description     = "Allow MySQL Traffic from the above Web App Security Group"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
     security_groups = [aws_security_group.application_security_group.id]
   }
 
+  #checkov:skip=CKV_AWS_382:Database needs outbound for AWS API calls and managed service communication
   egress {
     description = "Allow all outbound connections"
     from_port   = 0
     to_port     = 0
-    protocol    = "-1" // -1 refers to All Outbound Traffic being allowed, Across both TCP & UDP Suites.
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
     Name = "database-security-group"
   }
-
 }
